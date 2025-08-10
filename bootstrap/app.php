b9a97bb62a95fa72d7b464ec3618d563876a9131
agussuartawan <kadeksuwar@gmail.com>
2025-08-10 08:21:53 +0800

    try to fix http warn on vercel deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
+
+        // Trust semua proxy dan baca header X-Forwarded-* agar Laravel ngerti HTTPS dari Vercel
+        $middleware->trustProxies(
+            trustAll: true,
+            headers: TrustProxies::HEADER_X_FORWARDED_ALL
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
